c35fdc7a96d7fffc445592a52f2451fee0ad246a
Kateryna Derxen <92515141+Katerix@users.noreply.github.com>
2023-04-09 16:11:55 +0300

    lab3

---
+++ Labs/Lab3/Program.cs
@@ -0,0 +1,62 @@
+﻿using Lab3;
+using System.Diagnostics;
+
+// Знайти кількість непарних елементів в масиві та найбільше непарне число.
+
+public static class Program
+{
+    public static int N = 100000000;
+    public static int R = 100;
+    public static int _threadAmount = 8;
+
+    static Stopwatch timer = new Stopwatch();
+    static Stopwatch timer2 = new Stopwatch();
+    static Stopwatch timer3 = new Stopwatch();
+    static Stopwatch timer4 = new Stopwatch();
+
+
+    static void Main(string[] ags)
+    {
+        Console.WriteLine($"Given an integer array (N = {N}). Count the amount of odd number and a max odd value.\n");
+
+        var numbers = Services.RandomInit(R, N).ToArray();
+
+        Console.WriteLine("1. Single thread solution");
+
+        timer.Start();
+        var result = numbers.OddCountAndMaxSingleThread();
+        timer.Stop();
+
+        PrintResults(result, timer);
+
+        Console.WriteLine("2. Multithread solution using mutex");
+
+        timer2.Start();
+        result = numbers.CountOddAndMaxParallelWithLock(_threadAmount);
+        timer2.Stop();
+
+        PrintResults(result, timer2);
+
+        Console.WriteLine("3. Multithread solution using atomic operations");
+
+        timer3.Start();
+        result = numbers.OddCountAndMaxParallelWithAtomic(_threadAmount);
+        timer3.Stop();
+
+        PrintResults(result, timer3);
+
+        Console.WriteLine("4. Multithread solution using parallel LINQ");
+
+        timer4.Start();
+        result = numbers.OddCountAndMaxParallelLib();
+        timer4.Stop();
+
+        PrintResults(result, timer4);
+    }
+
+    static void PrintResults((int,int) result, Stopwatch timer)
+    {
+        Console.WriteLine($"Result: \nOdd numbers amount: {result.Item1} Max: {result.Item2}");
+        Console.WriteLine($"Took: {timer.ElapsedMilliseconds} ms. \n");
+    }
+}
---
+++ Labs/Lab3/Services.cs
@@ -0,0 +1,105 @@
+﻿using System.Threading;
+
+namespace Lab3
+{
+    public static class Services
+    {
+        static object _lock = new object();
+
+        public static IEnumerable<int> RandomInit(int range, int size)
+        {
+            var rand = new Random();
+
+            for (int i = 0; i < size; i++)
+                yield return rand.Next(range);
+        }
+
+        public static (int, int) OddCountAndMaxSingleThread(this int[] numbers)
+        {
+            int oddCount = 0, oddMax = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    oddCount++;
+
+                    if (numbers[i] > oddMax) oddMax = numbers[i];
+                }
+            }
+
+            return (oddCount, oddMax);
+        }
+
+        public static (int, int) CountOddAndMaxParallelWithLock(this int[] numbers, int threadsAmount)
+        {
+            int size = numbers.Length;
+            int chunk = size / threadsAmount;
+
+            int oddCount = 0, oddMax = 0;
+
+            Thread[] threads = new Thread[threadsAmount];
+
+            for (int i = 0; i < threadsAmount; i++)
+            {
+                int start = i * chunk;
+                int end = i == threadsAmount - 1 ? size : start + chunk;
+
+                threads[i] = new Thread(() => CountOddAndMaxSubmethod(numbers, start, end, ref oddCount, ref oddMax));
+            }
+
+            foreach (var thread in threads) thread.Start();
+
+            foreach (var thread in threads) thread.Join();
+
+            return (oddCount, oddMax);
+        }
+
+        private static void CountOddAndMaxSubmethod(int[] numbers, int start, int end, ref int oddCount, ref int oddMax)
+        {
+            for (int i = start; i < end; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    lock (_lock)
+                    {
+                        oddCount++;
+
+                        if (numbers[i] > oddMax)
+                        {
+                            oddMax = numbers[i];
+                        }
+                    }
+                }
+            }
+        }
+
+        public static (int, int) OddCountAndMaxParallelWithAtomic(this int[] numbers, int threadsAmount)
+        {
+            int oddCount = 0, oddMax = 0;
+
+            Parallel.ForEach(numbers, (number) =>
+            {
+                if (number % 2 != 0)
+                {
+                    Interlocked.Increment(ref oddCount);
+
+                    if (number > oddMax)
+                    {
+                        Interlocked.Exchange(ref oddMax, number);
+                    }
+                }
+            });
+
+            return (oddCount, oddMax);
+        }
+
+
+        public static (int, int) OddCountAndMaxParallelLib(this IEnumerable<int> numbers)
+        {
+            var odds = numbers.AsParallel().Where(n => n % 2 != 0);
+
+            return (odds.AsParallel().Count(), odds.AsParallel().Max());
+        }
+
+    }
+}
